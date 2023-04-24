f072541aca286874c0bc367934dd344fffe8239b
senseiuc <dhinhojboy@gmail.com>
2023-04-24 20:06:23 +0000

    created a json file of all users and their tasks

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Gets a to-do list for a given id
+"""
+import json
+import requests
+import sys
+
+
+def tasks_done(id):
+    '''
+    Script that displays an employee completed tasks
+    Parameters:
+    employee_id: Is an interger representing an employee id.
+    '''
+
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+    res = requests.get(url)
+    res_js = res.json()
+    emp_name = res_js.get("name")
+
+    url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
+    todos = requests.get(url)
+    todos_js = todos.json()
+    tasks_list = []
+
+    for task in todos_js:
+        task_dict = {}
+        task_dict["task"] = task.get("title")
+        task_dict["completed"] = task.get("completed")
+        task_dict["username"] = emp_name
+        tasks_list.append(task_dict)
+
+        todos = {str(id): tasks_list}
+        file_name = str(id)+".json"
+
+        with open(file_name, "a") as f:
+            json.dump(todos, f)
+
+
+if __name__ == "__main__":
+    tasks_done(sys.argv[1])
---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+Gets a to-do list for a given id
+"""
+import json
+import requests
+import sys
+
+
+def tasks_done():
+    '''
+    Script that displays an employee completed tasks
+    Parameters:
+    employee_id: Is an interger representing an employee id.
+    '''
+
+    id = 1
+    all_tasks_dict = {}
+
+    while True:
+        url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+        res = requests.get(url)
+        res_js = res.json()
+        if len(res_js) == 0:
+            break
+        emp_name = res_js.get("name")
+
+        url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
+        todos = requests.get(url)
+        todos_js = todos.json()
+        tasks_list = []
+
+        for task in todos_js:
+            task_dict = {}
+            task_dict["username"] = emp_name
+            task_dict["task"] = task.get("title")
+            task_dict["completed"] = task.get("completed")
+            tasks_list.append(task_dict)
+
+        all_tasks_dict[id] = tasks_list
+        id += 1
+
+    file_name = "todo_all_employees.json"
+    with open(file_name, "a") as f:
+        json.dump(all_tasks_dict, f)
+
+
+if __name__ == "__main__":
+    tasks_done()
