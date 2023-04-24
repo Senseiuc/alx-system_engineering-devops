07f7699605296b875fee15af7c1de0a1b22956ad
senseiuc <dhinhojboy@gmail.com>
2023-04-24 22:03:29 +0000

    fixed 3-dictionary of list

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -14,28 +14,29 @@ def tasks_done(id):
     employee_id: Is an interger representing an employee id.
     '''
 
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+    url = "https://jsonplaceholder.typicode.com/users?id={}".format(id)
     res = requests.get(url)
     res_js = res.json()
-    emp_name = res_js.get("username")
+    emp_name = res_js[0].get("username")
 
-    url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
+    url = "https://jsonplaceholder.typicode.com/todos".format(id)
     todos = requests.get(url)
     todos_js = todos.json()
     tasks_list = []
 
     for task in todos_js:
-        task_dict = {}
-        task_dict["task"] = task.get("title")
-        task_dict["completed"] = task.get("completed")
-        task_dict["username"] = emp_name
-        tasks_list.append(task_dict)
+        if task.get("userId") == int(id):
+            task_dict = {}
+            task_dict["task"] = task.get("title")
+            task_dict["completed"] = task.get("completed")
+            task_dict["username"] = emp_name
+            tasks_list.append(task_dict)
 
-        todos = {str(id): tasks_list}
-        file_name = str(id)+".json"
+    todos = {str(id): tasks_list}
+    file_name = str(id)+".json"
 
-        with open(file_name, "a") as f:
-            json.dump(todos, f)
+    with open(file_name, "a") as f:
+        json.dump(todos, f)
 
 
 if __name__ == "__main__":
--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -18,24 +18,25 @@ def tasks_done():
     all_tasks_dict = {}
 
     while True:
-        url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+        url = "https://jsonplaceholder.typicode.com/users?id={}".format(id)
         res = requests.get(url)
         res_js = res.json()
         if len(res_js) == 0:
             break
-        emp_name = res_js.get("username")
+        emp_name = res_js[0].get("username")
 
-        url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
+        url = "https://jsonplaceholder.typicode.com/todos"
         todos = requests.get(url)
         todos_js = todos.json()
         tasks_list = []
 
         for task in todos_js:
-            task_dict = {}
-            task_dict["username"] = emp_name
-            task_dict["task"] = task.get("title")
-            task_dict["completed"] = task.get("completed")
-            tasks_list.append(task_dict)
+            if task.get("userId") == int(id):
+                task_dict = {}
+                task_dict["username"] = emp_name
+                task_dict["task"] = task.get("title")
+                task_dict["completed"] = task.get("completed")
+                tasks_list.append(task_dict)
 
         all_tasks_dict[id] = tasks_list
         id += 1
