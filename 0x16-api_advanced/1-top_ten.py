d41e28b75457a0a1223748ac0a7b3a4d41bff232
senseiuc <dhinhojboy@gmail.com>
2023-05-09 10:54:06 +0000

    list first ten hot posts

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+a function that queries the Reddit API
+prints the titles of the first 10 hot posts
+listed for a given subreddit
+"""
+import requests
+
+
+def top_ten(subred):
+    """returns number of total subscribers"""
+    url = ("https://api.reddit.com/r/{}?sort=hot&limit=10".format(subred))
+    headers = {'User-Agent': 'RequestApi/1.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        print(None)
+        return
+    response = response.json()
+    if 'data' in response:
+        for posts in response.get('data').get('children'):
+            print(posts.get('data').get('title'))
+    else:
+        print(None)
